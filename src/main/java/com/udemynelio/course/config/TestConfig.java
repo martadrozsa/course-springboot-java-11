99d4fec49a75d23aea0a3b8991eed93214ffa7da
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-01 15:05:40 -0300

    Category

--- src/main/java/com/udemynelio/course/config/TestConfig.java
+++ src/main/java/com/udemynelio/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.udemynelio.course.config;
 
+import com.udemynelio.course.entities.Category;
 import com.udemynelio.course.entities.Order;
 import com.udemynelio.course.entities.User;
 import com.udemynelio.course.entities.enums.OrderStatus;
+import com.udemynelio.course.repositories.CategoryRepository;
 import com.udemynelio.course.repositories.OrderRepository;
 import com.udemynelio.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +21,14 @@ public class TestConfig implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
+    private final CategoryRepository categoryRepository;
 
 
     @Autowired
-    public TestConfig(UserRepository userRepository, OrderRepository orderRepository ) {
+    public TestConfig(UserRepository userRepository, OrderRepository orderRepository, CategoryRepository categoryRepository) {
         this.userRepository = userRepository;
         this.orderRepository = orderRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
@@ -34,9 +38,13 @@ public class TestConfig implements CommandLineRunner {
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 
 }
---
+++ src/main/java/com/udemynelio/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.udemynelio.course.services;
+
+import com.udemynelio.course.entities.Category;
+import com.udemynelio.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
