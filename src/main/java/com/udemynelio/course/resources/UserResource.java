5f9f319dd3d9df3e6ca2086f53588be3401485b8
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-01-01 09:34:37 -0300

    User entity and resource

---
+++ src/main/java/com/udemynelio/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.udemynelio.course.resources;
+
+import com.udemynelio.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "12345678", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
